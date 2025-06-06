c9091bfd8cee011108d230c8f1ad99501efac91a
Tinkermannn <wilmansaragih2@gmail.com>
2025-06-06 21:35:21 +0700

    Fix

--- BE/src/routes/user.route.js
+++ BE/src/routes/user.route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
-const authenticateJWT = require('../Middleware/Auth');
+const authenticateJWT = require('../Middleware/auth');
 const authorize = require('../Middleware/authorize');
 const { upload, handleUploadError } = require('../Middleware/upload');
 
