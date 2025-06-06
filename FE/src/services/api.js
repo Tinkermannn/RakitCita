51f68e5080b866fa3ed4149dde5b00f0dbce3e21
Tinkermannn <wilmansaragih2@gmail.com>
2025-06-06 22:03:35 +0700

    FIX

--- FE/src/services/api.js
+++ FE/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api/v1'; // Sesuaikan port jika berbeda
+const API_BASE_URL = import.meta.env.VITE_API_URL; // Sesuaikan port jika berbeda
 
 const apiClient = axios.create({
     baseURL: API_BASE_URL,
